a2ff6fc678f2d47195f10d62205111d65a31a71d
gram <mail@orsinium.dev>
2022-05-11 09:58:22 +0200

    basic eager execution

--- lib/re.ex
+++ lib/re.ex
@@ -3,6 +3,11 @@ defmodule Re do
   Documentation for `Re`.
   """
 
+  # Types of input and output:
+  # + `{:re_expr, string}` for something that needs to be grouped
+  # + `{:re_group, string}` for something that is already grouped
+  # + a random AST for something that must be handled in runtime.
+
   @doc """
   Hello world.
 
@@ -16,104 +21,144 @@ defmodule Re do
     :world
   end
 
-  defmacro beginning_of_line, do: "^"
-  defmacro end_of_line, do: "$"
-  defmacro anything, do: "."
-  defmacro literal(expr), do: expr
-
-  defmacro text(expr) when is_bitstring(expr), do: Regex.escape(expr)
-
-  defmacro text(expr) do
+  defmacrop eager(params, do: block) do
     quote do
-      Regex.escape(unquote(expr))
+      if Macro.quoted_literal?(unquote(params)) do
+        {term, _} = unquote(block) |> Code.eval_quoted()
+        Macro.escape(term)
+      else
+        IO.inspect(unquote(params))
+        unquote(block)
+      end
     end
   end
 
-  defmacro sequence(expr) when is_list(expr), do: Enum.join(expr, "")
+  defmacro to_string(expr) do
+    eager [expr] do
+      quote do
+        case unquote(expr) do
+          {t, result} when is_atom(t) ->
+            result
 
-  defmacro sequence(expr) do
-    quote do
-      Enum.join(unquote(expr), "")
-    end
-  end
-
-  defmacro any_of(expr) when is_list(expr), do: Enum.join(expr, "|")
-
-  defmacro any_of(expr) do
-    quote do
-      Enum.join(unquote(expr), "|")
-    end
-  end
-
-  defmacro none_of(expr) when is_bitstring(expr), do: "[^#{expr}]"
-
-  defmacro none_of(expr) do
-    quote do
-      "[^#{unquote(expr)}]"
-    end
-  end
-
-  defmacro in_range(expr1, expr2) when is_bitstring(expr1) and is_bitstring(expr2) do
-    "#{expr1}-#{expr2}"
-  end
-
-  defmacro in_range(expr1, expr2) do
-    quote do
-      "#{unquote(expr1)}-#{unquote(expr2)}"
-    end
-  end
-
-  defmacro zero_or_more(expr) do
-    quote do
-      "#{unquote(expr)}*"
-    end
-  end
-
-  defmacro one_or_more(expr) do
-    quote do
-      "#{unquote(expr)}+"
-    end
-  end
-
-  defmacro maybe(expr) do
-    quote do
-      "#{unquote(expr)}?"
-    end
-  end
-
-  defmacro repeated(expr, n) do
-    quote do
-      "#{unquote(expr)}{#{unquote(n)}}"
-    end
-  end
-
-  defmacro repeated(expr, at_least, at_most) do
-    quote do
-      "#{unquote(expr)}{#{unquote(at_least)},#{unquote(at_most)}}"
+          result ->
+            result
+        end
+      end
     end
   end
 
   defmacro group(expr) do
-    quote do
-      "(?:#{unquote(expr)})"
+    eager [expr] do
+      quote do: "(?:#{unquote(expr)})"
+    end
+  end
+
+  defmacro beginning_of_line, do: "^"
+  defmacro end_of_line, do: "$"
+  defmacro anything, do: "."
+  defmacro literal(expr), do: {:expr, expr}
+
+  defmacro text(expr) do
+    eager [expr] do
+      quote do
+        {:expr, Regex.escape(unquote(expr))}
+      end
+    end
+  end
+
+  defmacro sequence(expr) do
+    eager [expr] do
+      quote do
+        Enum.join(unquote(expr), "")
+      end
+    end
+  end
+
+  defmacro any_of(expr) do
+    eager [expr] do
+      quote do
+        Enum.join(unquote(expr), "|")
+      end
+    end
+  end
+
+  defmacro none_of(expr) do
+    eager [expr] do
+      quote do
+        "[^#{unquote(expr)}]"
+      end
+    end
+  end
+
+  defmacro in_range(expr1, expr2) do
+    eager [expr1, expr2] do
+      quote do
+        "#{unquote(expr1)}-#{unquote(expr2)}"
+      end
+    end
+  end
+
+  defmacro zero_or_more(expr) do
+    eager [expr] do
+      quote do
+        "#{unquote(expr)}*"
+      end
+    end
+  end
+
+  defmacro one_or_more(expr) do
+    eager [expr] do
+      quote do
+        "#{unquote(expr)}+"
+      end
+    end
+  end
+
+  defmacro maybe(expr) do
+    eager [expr] do
+      quote do
+        "#{unquote(expr)}?"
+      end
+    end
+  end
+
+  defmacro repeated(expr, n) do
+    eager [expr, n] do
+      quote do
+        "#{unquote(expr)}{#{unquote(n)}}"
+      end
+    end
+  end
+
+  defmacro repeated(expr, at_least, at_most) do
+    eager [expr, at_least, at_most] do
+      quote do
+        "#{unquote(expr)}{#{unquote(at_least)},#{unquote(at_most)}}"
+      end
     end
   end
 
   defmacro capture(expr) do
-    quote do
-      "(#{unquote(expr)})"
+    eager [expr] do
+      quote do
+        "(#{unquote(expr)})"
+      end
     end
   end
 
   defmacro capture(expr, name) do
-    quote do
-      "(?P<#{unquote(name)}>#{unquote(expr)})"
+    eager [expr, name] do
+      quote do
+        "(?P<#{unquote(name)}>#{unquote(expr)})"
+      end
     end
   end
 
   defmacro lazy(expr) do
-    quote do
-      "#{unquote(expr)}?"
+    eager [expr] do
+      quote do
+        "#{unquote(expr)}?"
+      end
     end
   end
 end
--- test/re_test.exs
+++ test/re_test.exs
@@ -2,33 +2,75 @@ defmodule ReTest do
   use ExUnit.Case
   doctest Re
 
-  describe "literal" do
-    test "explict string" do
-      assert Re.literal("hello?") == "hello?"
+  # describe "literal" do
+  #   test "explict string" do
+  #     assert Re.to_string(Re.literal("hello?")) == "hello?"
+  #   end
+  # end
+
+  # describe "text" do
+  #   test "explict string" do
+  #     assert Re.to_string(Re.text("hello?")) == ~S"hello\?"
+  #   end
+  # end
+
+  # describe "any_of" do
+  #   test "explict list" do
+  #     assert Re.to_string(Re.any_of(["a", "b"])) == "a|b"
+  #   end
+  # end
+
+  # describe "in_range" do
+  #   test "explict strings" do
+  #     assert Re.to_string(Re.in_range("a", "z")) == "a-z"
+  #   end
+  # end
+
+  # describe "zero_or_more" do
+  #   test "explict string" do
+  #     assert Re.to_string(Re.zero_or_more("a")) == "a*"
+  #   end
+  # end
+
+  # describe "maybe" do
+  #   test "explict string" do
+  #     assert Re.to_string(Re.maybe("a")) == "a?"
+  #   end
+  # end
+
+  # describe "repeated" do
+  #   test "explict string" do
+  #     assert Re.to_string(Re.repeated("a", 5)) == "a{5}"
+  #     assert Re.to_string(Re.repeated("a", 5, 10)) == "a{5,10}"
+  #   end
+  # end
+
+  describe "group" do
+    # test "explict string" do
+    #   assert "a" |> Re.group() |> Re.to_string() == "(?:a)"
+    # end
+
+    test "explict string is statically expanded" do
+      ast = quote do: Re.group(Re.group("a"))
+      assert Macro.expand(ast, __ENV__) == "(?:a)"
     end
+
+    #   test "variable" do
+    #     var = "a"
+    #     assert Re.to_string(Re.group(var)) == "(?:a)"
+    #   end
   end
 
-  describe "text" do
-    test "explict string" do
-      assert Re.text("hello?") == ~S"hello\?"
-    end
-  end
+  # describe "capture" do
+  #   test "explict string" do
+  #     assert Re.to_string(Re.capture("a")) == "(a)"
+  #     assert Re.to_string(Re.capture("a", "hello")) == "(?P<hello>a)"
+  #   end
+  # end
 
-  describe "any_of" do
-    test "explict list" do
-      assert Re.any_of(["a", "b"]) == "a|b"
-    end
-  end
-
-  describe "in_range" do
-    test "explict strings" do
-      assert Re.in_range("a", "z") == "a-z"
-    end
-  end
-
-  describe "zero_or_more" do
-    test "explict string" do
-      assert Re.zero_or_more("a") == "a*"
-    end
-  end
+  # describe "lazy" do
+  #   test "explict string" do
+  #     assert Re.to_string(Re.lazy(Re.one_or_more("a"))) == "a+?"
+  #   end
+  # end
 end
